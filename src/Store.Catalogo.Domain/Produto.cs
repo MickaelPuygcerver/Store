e9df4cafd00ac427e5682e05ff686129ac1759d6
MickaelPuygcerver <mickael.andrade@peakinvest.com.br>
2020-06-26 01:13:44 -0300

    validação de entidades

---
+++ src/Store.Catalogo.Domain/Produto.cs
@@ -0,0 +1,81 @@
+﻿using Store.Core.DomainObjects;
+using System;
+
+namespace Store.Catalogo.Domain
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        // Entidades devem possuir set privado
+        public Guid CategoriaId { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public string Imagem { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+        public Categoria Categoria { get; private set; }
+
+
+        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            CategoriaId = categoriaId;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+
+            // *Fail Fast Validation* Validando dentro do contrutor antes de mesmo da instância do objeto ser criada
+            // Se algo vai falhar que falhe o mais rápido possível.
+            Validar();
+        }
+
+        // AdHoc Setters
+        // Setar propriedades somente por AdHoc setters ou contrutores
+        public void Ativar() => Ativo = true;
+
+        public void Desativar() => Ativo = false;
+
+        public void AlterarCategorio(Categoria categoria)
+        {
+            Categoria = categoria;
+            CategoriaId = categoria.Id;
+        }
+
+        public void AlterarDescricao(string descricao)
+        {
+            Validacoes.ValidarSeVazio(descricao, "O campo Descrição do produto não pode estar vazio");
+            Descricao = descricao;
+        }
+
+        public void DebitarEstoque(int quantidade)
+        {
+            // Por algum motivo pode ter vindo um número negativo
+            if (quantidade < 0) quantidade *= -1;
+            if (PossuiEstoque(quantidade) == false) throw new DomainException("Estoque insuficiente");
+            QuantidadeEstoque -= quantidade;
+        }
+
+        public void ReporEstoque(int quantidade)
+        {
+            QuantidadeEstoque += quantidade;
+        }
+
+        public bool PossuiEstoque(int quantidade)
+        {
+            return QuantidadeEstoque >= quantidade;
+        }
+
+        // A entidade deve se auto validar
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome do produto não pode estar vazio");
+            Validacoes.ValidarSeVazio(Descricao, "O campo Descricao do produto não pode estar vazio");
+            Validacoes.ValidarSeIgual(CategoriaId, Guid.Empty, "O campo CategoriaId do produto não pode estar vazio");
+            Validacoes.ValidarSeMenorQue(Valor, 1, "O campo Valor do produto não pode se menor igual a 0");
+            Validacoes.ValidarSeVazio(Imagem, "O campo Imagem do produto não pode estar vazio");
+        }
+    }
+}
---
+++ src/Store.Core/DomainObjects/Entity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Store.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        // Com Id Guid podemos saber o Id da entidade antes dela ser persistida
+        public Guid Id { get; set; }
+
+        // Constutor protected já que a classe é abstrata
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 752) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            // Interpolação
+            return $"{GetType().Name} [Id={Id}]";
+        }
+    }
+}
